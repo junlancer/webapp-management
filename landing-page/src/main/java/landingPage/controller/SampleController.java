e00f01389dbd37c3b9332ac7d795e3cf09e0117c
junlancer <junlancer@163.com>
2019-09-28 13:08:09 +0800

    初始化

---
+++ landing-page/src/main/java/landingPage/controller/SampleController.java
@@ -0,0 +1,26 @@
+package landingPage.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @Description SampleController
+ * @Author Administrator
+ * @Date 2019/9/10 15:14
+ * @Version 1.0
+ **/
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+    @RequestMapping("/")
+    @ResponseBody
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(SampleController.class, args);
+    }
+}
